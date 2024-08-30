b33cf28b3c98053f456aa1b99830587648ade477
apoorva-info <apoorvaagupta.info@gmail.com>
2024-08-30 14:40:39 +0200

    Day 4 code uploaded

---
+++ Day 4/PatternProblem.py
@@ -0,0 +1,20 @@
+num = int(input("Enter a number: "))
+
+def pattern19(a):
+        for i in range(1,a+1):
+                for j in range(1,i+1):
+                    print("*", end="")
+                print(" " * (2*(a-i)),end="")
+                for j in range(1,i+1):
+                    print("*", end="")
+                print()
+                    
+                        
+            
+        print()
+        
+        
+
+
+    
+pattern19(num)
