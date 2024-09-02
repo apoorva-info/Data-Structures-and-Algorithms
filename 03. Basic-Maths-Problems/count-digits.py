01ace832eb2289492de562a3b8e0f885af1f5192
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-02 12:28:57 +0200

    count-digit problem: added comments

--- 03. Basic-Maths-Problems/count-digits.py
+++ 03. Basic-Maths-Problems/count-digits.py
@@ -1,11 +1,23 @@
+# Program to count the number of digits in a number entered by the user.
+
+# Prompt the user to enter a number and store it as an integer in 'num'
 num = int(input("Enter a number: "))
 
+# Example:
+# Input: 7789
+# Output: "The number of digits in the given number is 4"
+
+
+# Define a function to count the number of digits in a given number
 def countDigits(a):
-    count=0
-    while(a>0): #7789
-        n = a%10 #9
-        count=count+1
-        a = a//10 #778
-    print("The number of digits in the given numbers is ",count)
-    
+    count = 0  # Initialize a counter to zero
+    while(a > 0):  
+        n = a % 10  # Get the last digit of the number (not used further in the logic)
+        count = count + 1  # Increment the count by 1 for each digit
+        a = a // 10  # Remove the last digit from the number
+    print("The number of digits in the given number is", count)  
+
+
+
+# Call the function with the user-provided number
 countDigits(num)
