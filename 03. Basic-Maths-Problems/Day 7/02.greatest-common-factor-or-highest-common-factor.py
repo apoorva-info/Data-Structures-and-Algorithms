4b8f1a02e247c115f94a6df030001b50d54b3a62
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-03 16:46:17 +0200

    Modified GCF problem - Used Euclidean Algorithm

--- 03. Basic-Maths-Problems/Day 7/02.greatest-common-factor-or-highest-common-factor.py
+++ 03. Basic-Maths-Problems/Day 7/02.greatest-common-factor-or-highest-common-factor.py
@@ -8,7 +8,7 @@ first_input, second_input = map(int, input("Enter two numbers separated by a spa
 # Function to find the greatest common factor (GCF) of two numbers
 
 def greatest_common_factor(num1, num2):
-    n = max(num1, num2) # Find the maximum of the two numbers to determine the range of divisors
+    n = min(num1, num2) # Find the minimum of the two numbers to determine the range of divisors
     divisors = [] # Initialize an empty list to store common divisors of the two numbers
 
     for i in range(1, n + 1):
@@ -20,3 +20,43 @@ def greatest_common_factor(num1, num2):
 
 # Call the function with the user-provided numbers
 greatest_common_factor(first_input, second_input)
+
+# Time Complexity = O(min(first_value , second_value))
+
+
+
+# Function to find the greatest common factor (GCF) of two numbers using the Euclidean algorithm
+"""Euclidean Algorithm: A method to find the GCF of two numbers by repeatedly
+replacing the larger number with the remainder when it is divided by the smaller number.
+The process continues until one of the numbers becomes zero. The non-zero number is the GCF.
+GCF(a , b) = GCF(b , a % b) """
+
+# Example:
+# Input 1: 12 18
+# Output 1: "The greatest common factor of 12 and 18 is 6"
+
+
+def gcd_using_euclidean_algo(num1, num2):
+   
+    while num1 > 0 and num2 > 0:
+        if num1 > num2:
+            num1 = num1 % num2
+        else:
+            num2 = num2 % num1
+    
+    if num1 == 0:
+        return print(f"The greatest common factor of {first_input} and {second_input} is {num2}")
+    return print(f"The greatest common factor of {first_input} and {second_input} is {num1}")
+
+# Call the function with the user-provided numbers
+gcd_using_euclidean_algo(first_input, second_input)
+
+# Time Complexity: O(log_ϕ(min(num1, num2)))
+# Explanation: The time complexity is logarithmic relative to the size of the smaller input,
+# which makes the Euclidean algorithm very efficient for large numbers.
+
+
+        
+    
+
+
