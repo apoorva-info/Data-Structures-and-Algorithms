7896bcb9411abb13c3974ab0e7929747251b0e34
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-05 18:47:09 +0200

    Updated Structure

---
+++ 04. Recursion/Day 9/04. reverse-an-array-using-two-pointers.py
@@ -0,0 +1,48 @@
+# Function to reverse an array using Recursion(Using Two Pointers)
+# Example:
+# Input:
+# Enter the size of the array: 5
+# Enter the element for array[1]: 1
+# Enter the element for array[2]: 7
+# Enter the element for array[3]: 2
+# Enter the element for array[4]: 9
+# Enter the element for array[5]: 2
+# Output:
+# Original array:  [1, 7, 2, 9, 2]
+# Reverse array:  [2, 9, 2, 7, 1]
+
+def reverse_array(array, left, right):
+    if left >= right: # Base case: if the left index is greater than or equal to the right index, stop the recursion
+        return 
+    else:
+        # Initially, I created a swapping function separately. However, python provides a direct option for swapping.
+        # a,b = b,a
+        array[left] , array[right] = array[right] , array[left]
+        # swap(array, left, right) # Swap the elements at the left and right indices
+        reverse_array(array, left + 1, right - 1) # Recursive call to reverse the next pair of elements
+
+# # Function to swap the elements at the specified indices
+# def swap(array, left, right):
+#     temp = array[left]
+#     array[left] = array[right]
+#     array[right] = temp
+
+# Prompt the user to enter the size of the array
+array = []
+size = int(input("Enter the size of the array: "))
+for i in range(size): 
+    element = int(input(f"Enter the element for array[{i+1}]: "))
+    array.append(element)
+
+# Print the original array
+print("Original array: ", array)
+
+# Call the function to reverse the array
+reverse_array(array, 0, len(array) - 1)
+
+# Print the reversed array
+print("Reverse array: ", array)
+
+#  Time Complexity = O(n)
+
+
