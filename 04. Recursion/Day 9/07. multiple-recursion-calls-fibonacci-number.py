6a9fc9500d3f1ed8e04a02d137fdc2450050f400
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-05 20:51:17 +0200

    Added Fibonacci Number Problem

---
+++ 04. Recursion/Day 9/07. multiple-recursion-calls-fibonacci-number.py
@@ -0,0 +1,25 @@
+# Recursive function to calculate the Fibonacci number at a given position 'a'
+# The Fibonacci sequence is defined as:
+# F(0) = 0, F(1) = 1, and F(n) = F(n-1) + F(n-2) for n > 1
+# Example:
+# Input: Enter a number: 5
+# Output: The Fibonacci number at position 5 is 5.
+
+
+def fibonacci_number(a):
+    if a <= 0: # Base Case
+        return 0
+    elif a == 1: # Base Case
+        return 1
+    else:
+        # Recursive call to calculate the Fibonacci number using the sum of the two preceding numbers
+        return fibonacci_number(a - 1) + fibonacci_number(a - 2)
+
+# Prompt the user to enter a position number
+num = int(input("Enter a number: "))  
+
+# Call the function to get the Fibonacci number at the given position
+result = fibonacci_number(num)
+
+# Print the result
+print(f"The Fibonacci number at position {num} is {result}.")
