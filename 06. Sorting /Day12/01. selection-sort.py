5f92f37a9c74c1dffb15ce56208cebe60f26e044
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-09 19:36:44 +0200

    Updated Code

--- 06. Sorting /Day12/01. selection-sort.py
+++ 06. Sorting /Day12/01. selection-sort.py
@@ -13,24 +13,27 @@
 # Output:
 # Sorted Array: [11, 12, 22, 25, 64]
 
-# Prompt the user to enter the size of the array
+# Selection Sort Algorithm
+def selection_sort(array):
+    for i in range(len(array)):
+        min_value_index = i 
+        # Finding the minimum element in the unsorted part of the array
+        for j in range(i + 1, len(array)): 
+            if array[j] < array[min_value_index]:
+                min_value_index = j  
+        # Swapping
+        array[i], array[min_value_index] = array[min_value_index], array[i]
+
+# User Input
 size = int(input("Enter the size of the array: "))
 array = []
-
-# Loop to take array elements as input from the user
 for i in range(size):
     element = int(input(f"Enter the element at array[{i}]: "))
     array.append(element)
 
-# Selection Sort Algorithm
-for i in range(size):
-    min_value_index = i 
-    # Finding the minimum element in the unsorted part of the array
-    for j in range(i + 1, size): 
-        if array[j] < array[min_value_index]:
-            min_value_index = j  
-    # Swapping
-    array[i], array[min_value_index] = array[min_value_index], array[i]
-
+# Function Call
+selection_sort(array)
 # Print the sorted array
 print(f"Sorted Array: {array}")
+
+# Time Complexity = O(n^2)
