e75efdbc5cdcdba6066bc30a7e2677aa152ea186
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-14 22:50:10 +0200

    Updated Brute Force Approach to the problem

--- 07. Arrays/Day 17/01. left-rotate-by-d-places.py
+++ 07. Arrays/Day 17/01. left-rotate-by-d-places.py
@@ -1,9 +1,14 @@
 from array import array
-def left_rotate_by_d_places(array,n):
-    temp = array[0]
-    for i in range(len(array)-n):
-        array[i] = array[i+n]
-    array[len(array)-n] = temp
+# Brute Force Approach
+def left_rotate_by_d_places_brute(array,k):
+    temp = []
+    i = 0
+    for i in range(k):
+            temp.append(array[i])
+    for i in range(k,len(array)):
+        array[i-k] = array[i]
+    for i in range(k):
+        array[len(array)-k+i] = temp[i]
     print(array)
 
     
@@ -19,6 +24,6 @@ for i in range(size):
     element = int(input(f"Enter the element at array[{i}]: "))
     user_array.append(element)
 d = int(input("Enter the number to which your array should be rotated: "))
+d = d % size
 print(user_array)
-
-left_rotate_by_d_places(user_array,d)
+left_rotate_by_d_places_brute(user_array,d)
