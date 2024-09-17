478754aa10cf5e346138deee9965a4afa5f76e8c
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-17 19:15:48 +0200

    Added find missing number in an array : Optimal Approach 2

--- 07. Arrays/Day 19/01. find-missing-number-in-an-array.py
+++ 07. Arrays/Day 19/01. find-missing-number-in-an-array.py
@@ -1,3 +1,5 @@
+# Function to find the missing number in an array
+
 from array import array
 
 # Brute Force Approach 1
@@ -46,18 +48,31 @@ def missing_number_using_sum(arr,n):
 # Time Complexity = O(n)
 # Space Complexity = O(1)
 
-
-
+# Optimal Approach 2
+def missing_number_using_xor(arr,n):
+    xor1 = 0 
+    for i in range(1,n+1):
+        xor1 = xor1 ^ i
+    xor2 = 0 
+    for i in arr:
+        xor2 = xor2 ^ i
+    return xor1^xor2
+# Time Complexity = O(n)
+# Space Complexity = O(1)
 
 
 # User Input
 user_array = array('i',[2,5,4,3,1,7])
 len_of_user_array = 7
+
+# Function Call
 # result1 = missing_number_brute(user_array,len_of_user_array)
 # print(f"Missing number: {result1}")
 # result2 = missing_number(user_array,len_of_user_array)
 # print(f"Missing number: {result2}")
 # result3 = missing_number_using_hashing(user_array,len_of_user_array)
 # print(f"Missing number: {result3}")
-result4 = missing_number_using_sum(user_array,len_of_user_array)
-print(f"Missing number: {result4}")
+# result4 = missing_number_using_sum(user_array,len_of_user_array)
+# print(f"Missing number: {result4}")
+result5 = missing_number_using_xor(user_array,len_of_user_array)
+print(f"Missing number: {result5}")
