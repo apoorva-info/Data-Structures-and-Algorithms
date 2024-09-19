2214e945787b434c9092aa9b2fa732261a4ddafe
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-19 15:45:37 +0200

    Updated optimal approach :  longest subarray with sum k problem

--- 07. Arrays/Day 20/01. longest-subarray-with-sum-k.py
+++ 07. Arrays/Day 20/01. longest-subarray-with-sum-k.py
@@ -13,8 +13,6 @@ def longest_sub_array_brute(arr,k):
 # Time Complexity = O(n^3)
 # Space Complexity = O(1)
 
-
-
 # Better Solution
 def longest_sub_array_better(arr,k):
     length = 0
@@ -29,6 +27,23 @@ def longest_sub_array_better(arr,k):
 # Time Complexity = O(n^2)
 # Space Complexity = O(1)
 
+# Optimal Solution
+def longest_sub_array_optimal(arr,k):
+    sum_map = {}
+    max_length = 0
+    sum = 0
+    for i in range(len(arr)):
+        sum = sum + arr[i]
+        if sum == k:
+            max_length = i+1
+        if (sum - k) in sum_map:
+            max_length = max(max_length , i - sum_map[sum - k])
+        if sum not in sum_map:
+            sum_map[sum] = i
+
+    return max_length
+# Time Complexity = O(n)
+# Space Complexity = O(n)
 
         
 
@@ -40,5 +55,9 @@ for i in range(n):
     element = int(input(f"Enter the element of the array[{i}]: "))
     user_array.append(element)
 k = int(input("Enter the value of k: "))
-result = longest_sub_array_brute(user_array,k)
-print(f"The length of the longest sub-array with sum {k} is {result}.")
+result1 = longest_sub_array_brute(user_array,k)
+print(f"The length of the longest sub-array with sum {k} is {result1}.")
+result2 = longest_sub_array_better(user_array,k)
+print(f"The length of the longest sub-array with sum {k} is {result2}.")
+result3 = longest_sub_array_optimal(user_array,k)
+print(f"The length of the longest sub-array with sum {k} is {result3}.")
