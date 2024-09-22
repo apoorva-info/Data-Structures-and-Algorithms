afcb908be01db4599f4efc2a58d5ad19da0fa45d
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-22 15:57:19 +0200

    Updated code

--- 07. Arrays/Day 22/01. majority-element-that-appears-more-than-n/2-times.py
+++ 07. Arrays/Day 22/01. majority-element-that-appears-more-than-n/2-times.py
@@ -1,19 +1,36 @@
+# Goal: This code finds the majority element in an array, where the majority element is defined as an element
+# that appears more than n/2 times in the array. Three approaches are used: brute force, a better approach using hashing,
+# and the optimal solution using Moore's Voting Algorithm.
+
+# Example:
+# Input:
+# Enter the size of the array: 7
+# Enter the element at array[0]: 3
+# Enter the element at array[1]: 3
+# Enter the element at array[2]: 4
+# Enter the element at array[3]: 2
+# Enter the element at array[4]: 3
+# Enter the element at array[5]: 3
+# Enter the element at array[6]: 3
+# Output:
+# Majority Element: 3
+
 from array import array
+
 # Brute Force Approach
-def majority_element_brute(arr,n):
+def majority_element_brute(arr, n):
     for i in range(n):
         count = 0
         for j in range(n):
             if arr[i] == arr[j]:
                 count += 1
-                if count > n//2:
+                if count > n // 2:
                     return arr[i]
 # Time Complexity = O(n^2)
 # Space Complexity = O(1)
 
-                
 # Better Approach
-def majority_element_better(arr,n):
+def majority_element_better(arr, n):
     max_element = max(arr)
     hash_arr = [0] * (max_element+1)
     for i in range(n):
@@ -27,7 +44,7 @@ def majority_element_better(arr,n):
 
 # Optimal Approach
 # Moore"s Voting Algorithm
-def majority_element_optimal(arr,n):
+def majority_element_optimal(arr, n):
     count = 0
     for i in range(n):
         if count == 0:
@@ -40,21 +57,12 @@ def majority_element_optimal(arr,n):
     for i in range(n):
         if arr[i] == element:
             count1 += 1
-    if count1 > n//2:
+    if count1 > n // 2:
         return element
 # Time Complexity = O(n)
 # Space Complexity = O(1)
-    
 
-        
-
-
-
-
-
-
-
-
+# User Input
 user_array = array('i', [])
 size = int(input("Enter the size of the array: "))
 for i in range(size):
@@ -62,9 +70,12 @@ for i in range(size):
     user_array.append(element)
 print(user_array)
 
-# result = majority_element_brute(user_array,size)
+# Uncomment to run the desired approach
+# result = majority_element_brute(user_array, size)
 # print(result)
-# result = majority_element_better(user_array,size)
+
+# result = majority_element_better(user_array, size)
 # print(result)
-result = majority_element_optimal(user_array,size)
+
+result = majority_element_optimal(user_array, size)
 print(result)
