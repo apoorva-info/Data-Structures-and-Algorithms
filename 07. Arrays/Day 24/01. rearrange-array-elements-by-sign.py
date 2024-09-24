23051bdb87d83798d4c4b380f22be5d4ae8cb197
apoorva-info <apoorvaagupta.info@gmail.com>
2024-09-24 19:56:27 +0200

    Updated optimal approach

--- 07. Arrays/Day 24/01. rearrange-array-elements-by-sign.py
+++ 07. Arrays/Day 24/01. rearrange-array-elements-by-sign.py
@@ -1,6 +1,7 @@
+# Brute Force Approach
 def rearrange_elements_brute(arr,n):
     if n % 2 != 0:
-        return "The size should be even."
+        return "The size is invalid."
     else:
         positive_arr = []
         negative_arr = []
@@ -14,7 +15,30 @@ def rearrange_elements_brute(arr,n):
         for i in range(1,n//2,2):
                 arr[i] = negative_arr[i]
         return arr
-                
+# Time Complexity = O(n)
+# Space Complexity = O(n)
+
+# Optimal Approach
+def rearrange_elements_optimal(arr,n):
+    if n % 2 != 0:
+        return "The size is invalid."
+    else:
+        ans = [None] * n # The list should not be empty.
+        positive_index = 0
+        negative_index = 1
+        for i in range(n):
+            if arr[i] > 0:
+                ans[positive_index] = arr[i]
+                positive_index += 2
+            else:
+                ans[negative_index] = arr[i] 
+                negative_index += 2     
+        
+        return ans
+# Time Complexity = O(n)
+# Space Complexity = O(n)
+        
+     
 
                 
             
@@ -32,7 +56,9 @@ for i in range(size):
     array.append(element)
 print(array)
 
-result = rearrange_elements_brute(array,size)
-print(result)
+# result = rearrange_elements_brute(array,size)
+# print(result)
 
+result = rearrange_elements_optimal(array,size)
+print(result)
 
