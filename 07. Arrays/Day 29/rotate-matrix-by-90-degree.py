f9ed5fb098672bcca326973acb41dc97804c038b
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-10 13:36:25 +0200

    Updated rotate matrix by 90 degree problem: Optimal Approach

--- 07. Arrays/Day 29/rotate-matrix-by-90-degree.py
+++ 07. Arrays/Day 29/rotate-matrix-by-90-degree.py
@@ -13,21 +13,38 @@ def rotate_matrix(matrix,rows,columns):
 
     return ans
 # Time Complexity = O(n^2)
-# Space Complexity = O(n)
+# Space Complexity = O(n^2)
+
+# Optimal Approach: 1. Transpose 2. Reverse
+def rotate_matrix_optimal(matrix,n):
+    for i in range(n):
+        for j in range(i+1,n):
+                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+
+    for i in range(n):
+        for j in range(n//2):
+            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+    return matrix
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
+
 
 
 
 
 
 m = int(input("Enter the number of rows: "))
-n = int(input("Enter the number of columns: "))
+n = m
 
 matrix = np.zeros((m,n),dtype=int)
 
 for i in range(m):
     for j in range(n):
         matrix[i][j] = int(input(f"Enter the element at matrix[{i}][{j}]: "))
-print(matrix)
+print(f"Original matrix : \n{matrix}")
 
 result = rotate_matrix(matrix,m,n)
-print(result)
+print(f"Rotated matrix: \n{result}")
+
+result = rotate_matrix_optimal(matrix,m)
+print(f"Rotated matrix: \n{result}")
