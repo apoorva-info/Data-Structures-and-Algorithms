8a9f538a7e0fc95af274fbc9a24e416d018e4cf8
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-14 14:10:39 +0200

    Updated count-subarray-sum-equals-k problem: Added notes

--- 07. Arrays/Day 30/02. count-subarray-sum-equals-k.py
+++ 07. Arrays/Day 30/02. count-subarray-sum-equals-k.py
@@ -19,15 +19,28 @@ def number_of_subarrays_with_sum_k_better(arr,k,n):
         current_sum = 0
         for j in range(i,n):
             current_sum = current_sum + arr[j]
-            print(current_sum)
-            print()
             if current_sum == k:
                 count+=1
     return count
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
 
+# Optimal Approach
+from collections import defaultdict
+def number_of_subarrays_with_sum_k_optimal(arr,k,n):
+    mpp = defaultdict(int)
+    sum = 0
+    count = 0
+    mpp[0] = 1
 
-
-
+    for i in range(n):
+        sum = sum + arr[i]
+        remove = sum - k
+        count += mpp[remove] 
+        mpp[sum] += 1       
+    return count
+# Time Complexity = O(n)
+# Space Complexity = O(n)
 
 
 
@@ -54,4 +67,7 @@ result = number_of_subarrays_with_sum_k_brute(user_array,k,n)
 print(result)
 
 result = number_of_subarrays_with_sum_k_better(user_array,k,n)
+print(result)
+
+result = number_of_subarrays_with_sum_k_optimal(user_array,k,n)
 print(result)
