532d4b212efafcabbd883b0478cb83a978242545
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-15 12:31:54 +0200

    Optimized the solution using approach from problem 2

--- 07. Arrays/Day 31/03. pascal-triangle-problem3.py
+++ 07. Arrays/Day 31/03. pascal-triangle-problem3.py
@@ -1,15 +1,16 @@
 # Problem 3
 # Goal : To print the pascal triangle.
 
+# Using the approach used to solve problem 1.
 def nCr(n,r):
     res = 1
     for i in range(r):
         res = res * (n-i)
         res = res // (i+1)
     return int(res)
+# Time Complexity = O(r)
 
-
-def pascalTriangle(r):
+def pascalTriangle_brute(r):
     ans = []
     for i in range(r):
         temp = []
@@ -17,11 +18,31 @@ def pascalTriangle(r):
             temp.append(nCr(i,c))
         ans.append(temp)
     return ans
+# Time Complexity ~ O(n^3)
+# Space Complexity = O(n)
             
+# Using the approach stated in problem 2.
+def pascalTriangle_better(r):
+    ans = []
+    for row in range(1,r+1):
+        temp = []
+        previous_result = 1
+        temp.append(previous_result)
+        for c in range(1,row):
+            previous_result = (row - c) * previous_result
+            previous_result = previous_result // c
+            temp.append(previous_result)
+        ans.append(temp)
+    return ans
+
 
 
 
 
 row = int(input("Enter the number of rows: "))
-result = pascalTriangle(row)
+
+# result = pascalTriangle_brute(row)
+# print(result)
+
+result = pascalTriangle_better(row)
 print(result)
