dfba34502d8e1ea1b337fde4419427198d07b0ef
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-20 12:36:06 +0200

    Updated 4-sum problem: Added Better Approach

--- 06. Arrays/Day 33/01. 4-sum.py
+++ 06. Arrays/Day 33/01. 4-sum.py
@@ -1,3 +1,4 @@
+# Brute Force Approach
 def four_sum_brute(arr,n, target):
     ans = set()
     for i in range(n):
@@ -11,7 +12,25 @@ def four_sum_brute(arr,n, target):
                         ans.add(tuple(temp))
     return ans
 # Time Complexity = O(n^4)
-# Space Complexity = O(n)
+# Space Complexity = O(no. of quads)*2(in  which you store the answer and return)
+
+# Better Approach
+def four_sum_better(arr, n, target):
+    ans = set()
+    for i in range(n):
+        for j in range(i+1,n):
+            hash_set = set()
+            for k in range(j+1,n):
+                l = target - (arr[i] + arr[j] + arr[k])
+                if l in hash_set:
+                    temp = [arr[i],arr[j],arr[k],l]
+                    temp.sort()
+                    ans.add(tuple(temp))
+                hash_set.add(arr[k])
+    return ans
+# Time Complexity = O(n^3)
+# Space Complexity = O(no. of quads)*2(in  which you store the answer and return)
+            
 
 
 lst = []
@@ -23,7 +42,7 @@ target = int(input("Enter the value of the target: "))
 print(lst)
 
 # Uncomment the desired function call
-result = four_sum_brute(lst, n, target)
-# result = four_sum_better(lst, n)
+# result = four_sum_brute(lst, n, target)
+result = four_sum_better(lst, n, target)
 # result = four_sum_optimal(lst, n)
 print(result)
