fa0c80ee957c898bc2eae1461f5e827eab6259fa
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-21 19:34:50 +0200

    Updated number of subarrays with xor k problem: Optimal Approach

--- 06. Arrays/Day 34/01. number-of-subarrays- xor-k.py
+++ 06. Arrays/Day 34/01. number-of-subarrays- xor-k.py
@@ -25,6 +25,21 @@ def count_subarrays_with_xor_k_better(arr,n,k):
 # Time Complexity = O(n^2)
 # Space Complexity = O(1)
 
+from collections import defaultdict
+# Optimal Approach
+def count_subarrays_with_xor_k_optimal(arr,n,k):
+    count = 0
+    xr = 0
+    hash_map = defaultdict(int)
+    hash_map[xr] = 1
+    for i in range(n):
+        xr = xr ^ arr[i]
+        x = xr ^ k 
+        count += hash_map[x]
+        hash_map[xr] += 1
+    return count
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
 
 # User Input
 lst = []
@@ -38,5 +53,8 @@ print(lst)
 # result = count_subarrays_with_xor_k_brute(lst,n,k)
 # print(result)
 
-result = count_subarrays_with_xor_k_better(lst,n,k)
+# result = count_subarrays_with_xor_k_better(lst,n,k)
+# print(result)
+
+result = count_subarrays_with_xor_k_optimal(lst,n,k)
 print(result)
