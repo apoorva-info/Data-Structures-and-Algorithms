4b7c331cb71841df4658f29e924dd3831899f381
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-21 19:05:09 +0200

    Updated number of subarrays with xor k problem: Better Approach

--- 06. Arrays/Day 34/01. number-of-subarrays- xor-k.py
+++ 06. Arrays/Day 34/01. number-of-subarrays- xor-k.py
@@ -1,3 +1,4 @@
+# Brute Force Approach
 def count_subarrays_with_xor_k_brute(arr,n,k):
     count = 0
     for i in range(n):
@@ -11,6 +12,19 @@ def count_subarrays_with_xor_k_brute(arr,n,k):
 # Time Complexity = O(n^3)
 # Space Complexity = O(1)
 
+# Better Approach
+def count_subarrays_with_xor_k_better(arr,n,k):
+    count = 0
+    for i in range(n):
+        XOR = 0
+        for j in range(i,n):
+            XOR = XOR ^ arr[j]
+            if XOR == k:
+                count += 1
+    return count
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
+
 
 # User Input
 lst = []
@@ -21,5 +35,8 @@ for i in range(n):
 k = int(input("Enter the value of k: "))
 print(lst)
 
-result = count_subarrays_with_xor_k_brute(lst,n,k)
+# result = count_subarrays_with_xor_k_brute(lst,n,k)
+# print(result)
+
+result = count_subarrays_with_xor_k_better(lst,n,k)
 print(result)
