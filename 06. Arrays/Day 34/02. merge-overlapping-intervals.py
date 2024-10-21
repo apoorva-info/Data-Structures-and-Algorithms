83cb962f8422b7c76291f7fc71fa257d8d1e3044
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-21 21:50:01 +0200

    Added merge overlapping intervals problem: Optimal Approach

--- 06. Arrays/Day 34/02. merge-overlapping-intervals.py
+++ 06. Arrays/Day 34/02. merge-overlapping-intervals.py
@@ -13,11 +13,28 @@ def overlapping_intervals_brute(arr):
                 end = max(end , arr[j][1])
             else:
                 break
-        ans.append((start,end))
-
+        ans.append([start,end])
 
     return ans
-    
-intervals = [(1,3),(2,6),(8,9),(9,11),(8,10),(2,4),(15,18),(16,17)]
-result = overlapping_intervals_brute(intervals)
+# Time Complexity = O(n log n) + O(2n)
+# Space Complexity = O(n)
+
+def overlapping_intervals_optimal(arr):
+    arr = sorted(arr)
+    n = len(arr)
+    ans = []
+    for i in range(n):
+        if not ans or arr[i][0] > ans[len(ans)-1][1]:
+            ans.append(arr[i])
+        else:
+            ans[len(ans)-1][1] = max(ans[len(ans)-1][1],arr[i][1])
+    return ans
+
+# User Input   
+intervals = [[1,3],[2,6],[8,9],[9,11],[8,10],[2,4],[15,18],[16,17]]
+
+# result = overlapping_intervals_brute(intervals)
+# print(result)
+
+result = overlapping_intervals_optimal(intervals)
 print(result)
