ce22bcc0930e6ecc03e6388a4b85d71a26552657
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-23 20:59:51 +0200

    Calculated the time and space complexity

--- 06. Arrays/Day 36/02. reverse-pairs.py
+++ 06. Arrays/Day 36/02. reverse-pairs.py
@@ -1,3 +1,18 @@
+# Goal : The goal is to find the number of elements with reverse pairs i.e. if i < j then a[i] > 2 * a[j]
+# Example:
+# Input:
+# Enter the size of the array: 7
+# Enter the element at array[0]: 40
+# Enter the element at array[1]: 25
+# Enter the element at array[2]: 19
+# Enter the element at array[3]: 9
+# Enter the element at array[4]: 6
+# Enter the element at array[5]: 2
+# Enter the element at array[6]: 5
+# Output:
+# 15
+
+# Brute Force Approach
 def reverse_pairs_brute(arr,n):
     count = 0
     for i in range(n):
@@ -5,13 +20,16 @@ def reverse_pairs_brute(arr,n):
             if arr[i] > 2 * arr[j]:
                 count += 1
     return count
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
 
-
+# User Input
 size = int(input(f"Enter the size of the array: "))
 array = []
 for i in range(size):
     element = int(input(f"Enter the element at array[{i}]: "))
     array.append(element)
-
+    
+# Function Call
 result = reverse_pairs_brute(array,size)
 print(result)
