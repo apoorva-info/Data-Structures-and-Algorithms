d04547ddc6c7c492ebb062d4acf6ea53c3f79cd8
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-23 18:55:14 +0200

    Added count inversions in an array problem

---
+++ 06. Arrays/Day 36/01. count-inversions-in-an-array.py
@@ -0,0 +1,33 @@
+# Goal: The goal is to count the number of inversions in an array.
+# Example:
+# Input:
+# Enter the size of the list: 6
+# Enter the element at lst[0]: 5
+# Enter the element at lst[1]: 3
+# Enter the element at lst[2]: 2
+# Enter the element at lst[3]: 7
+# Enter the element at lst[4]: 5
+# Enter the element at lst[5]: 4
+# Output:
+# 7
+
+# Brute Force Approach
+def count_inversions_brute(arr,n):
+    count = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i] > arr[j]:
+                count += 1
+    return count
+
+
+# User Input
+lst = []
+n = int(input("Enter the size of the list: "))
+for i in range(n):
+    element = int(input(f"Enter the element at lst[{i}]: "))
+    lst.append(element)
+
+# Function Call
+result = count_inversions_brute(lst,n) 
+print(result)
