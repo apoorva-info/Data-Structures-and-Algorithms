b4cbe1005f5f639455a1342fbc3e2925071a08a8
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-24 16:29:18 +0200

    Added maximum product subarray problem

---
+++ 06. Arrays/Day 37/01. maximum-product-subarray.py
@@ -0,0 +1,17 @@
+# Goal: Given an array, find the maximum product subarray.
+
+# Brute Force Approach
+
+
+
+
+# User Input
+size = int(input(f"Enter the size of the array: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the element at array[{i}]: "))
+    array.append(element)
+
+# Function Call
+# result = maximum_product_subarray_brute(array,size)
+# print(result)
