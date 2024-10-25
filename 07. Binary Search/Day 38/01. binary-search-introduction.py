00c9d7a55699905ceb8179301ea0a0c793598eb1
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-25 17:29:10 +0200

    Added recursive code for binary search

--- 07. Binary Search/Day 38/01. binary-search-introduction.py
+++ 07. Binary Search/Day 38/01. binary-search-introduction.py
@@ -2,6 +2,18 @@
 # Search in a sorted area
 
 # Goal: Given a sorted array, return the index of a given target value.
+# Example:
+# Input:
+# Enter the size of the array: 6
+# Enter the element at array[0]: 1
+# Enter the element at array[1]: 3
+# Enter the element at array[2]: 5
+# Enter the element at array[3]: 7
+# Enter the element at array[4]: 9
+# Enter the element at array[5]: 10
+# Enter the value that you want to search: 7
+# Output:
+# 3
 
 # Linear Search
 def search_target_linear(arr,n,target):
@@ -35,16 +47,10 @@ def recursive_code(arr,low,high,target):
         if target == arr[mid]:
             return mid 
         elif target < arr[mid]:
-            high = mid - 1
+            return recursive_code(arr,low,mid-1,target)
         else:
-            low = mid + 1
+            return recursive_code(arr,mid+1,high,target)
         
-
-
-
-    
-
-
 # User Input
 size = int(input(f"Enter the size of the array: "))
 array = []
@@ -54,5 +60,9 @@ for i in range(size):
 target_value = int(input("Enter the value that you want to search: "))
 
 # Function Call
-result = search_target_linear(array,size,target_value)
+# result = search_target_linear(array,size,target_value)
+# print(result)
+# result = search_target_binary(array,size,target_value)
+# print(result)
+result = recursive_code(array,size,target_value)
 print(result)
