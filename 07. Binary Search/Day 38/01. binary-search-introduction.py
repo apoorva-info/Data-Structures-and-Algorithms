d2094bc796567e63792e42a9a4b8bdcb1f319678
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-25 16:53:12 +0200

    Used Iterative Approach

--- 07. Binary Search/Day 38/01. binary-search-introduction.py
+++ 07. Binary Search/Day 38/01. binary-search-introduction.py
@@ -12,27 +12,22 @@ def search_target_linear(arr,n,target):
 # Space Complexity = O(1)
 
 # Search space: Everything between low and high.
+# Iterative Approach
 def search_target_binary(arr,n,target):
     low = 0 
     high = n - 1
-    mid = (low + high)//2
-    while low != high != mid:
-        if target < arr[mid]:
+    while low <= high:
+        mid = (low + high//2)
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
             high = mid - 1
-            mid = (low + high)//2
-        elif target > arr[mid]:
+        else:
             low = mid + 1
-            mid = (low + high)//2
-    return low
-
-
-        
-
-
+    return -1
 
 
     
-
 
 
 # User Input
