fdfaa5b336f2802efe2962ae4075aac884b3f5eb
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-25 16:14:03 +0200

    index-of-the-target-value problem

---
+++ 07. Binary Search/Day 38/01. binary-search-introduction.py
@@ -0,0 +1,27 @@
+# Binary Search: A searching algorithm in a limited search space.
+# Search in a sorted area
+
+# Goal: Given a sorted array, return the index of a given target value.
+
+# Brute Force Approach --> Using Linear Search
+def search_target(arr,n,target):
+    for i in range(n):
+        if arr[i] == target:
+            return i
+# Time Complexity = O(n)
+# Space Complexity = O(1)
+
+
+
+
+# User Input
+size = int(input(f"Enter the size of the array: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the element at array[{i}]: "))
+    array.append(element)
+target_value = int(input("Enter the value that you want to search: "))
+
+# Function Call
+result = search_target(array,size,target_value)
+print(result)
