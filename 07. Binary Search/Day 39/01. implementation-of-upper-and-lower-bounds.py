78c750bf65eb70d1d6ee05a90387df991c350ae3
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-26 13:12:59 +0200

    Added implementation of lower bound problem

---
+++ 07. Binary Search/Day 39/01. implementation-of-upper-and-lower-bounds.py
@@ -0,0 +1,26 @@
+# Goal: To find the lower bound of the given number.
+# Lower Bound: The smallest index such that arr[i] >= x
+def lower_bound(arr,n,x):
+    low = 0
+    high = n - 1
+    ans = n - 1
+    while low < high:
+        mid = (low + high)//2 
+        if arr[mid] >= x:
+            ans = mid
+        elif arr[mid] < x:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return ans
+            
+# User Input
+size = int(input(f"Enter the size of the sorted array: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the element at array[{i}]: "))
+    array.append(element)
+x = int(input("Enter the value whose lower bound is to found: "))
+
+result = lower_bound(array,size,x)
+print(result)
