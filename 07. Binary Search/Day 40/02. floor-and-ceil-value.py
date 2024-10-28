bd3e93e40c612dff477164ed4b50d831f9f721d2
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-28 13:36:04 +0100

    Added: Floor and Ceil Value Problem

---
+++ 07. Binary Search/Day 40/02. floor-and-ceil-value.py
@@ -0,0 +1,28 @@
+# Goal: To find the floor(largest number <= x) and ceil(smallest number >= x) value n sorted array.
+
+def floor_value(arr,n,target):
+    low = 0
+    high = n - 1
+    ans = n
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            ans = mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return ans
+
+
+# User Input
+size = int(input(f"Enter the size of the sorted array: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the element at array[{i}]: "))
+    array.append(element)
+x = int(input("Enter the value of the target: "))
+
+# Function Call
+result = floor_value(array,size,x)
+print(result)
