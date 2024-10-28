c371a507afb5ef0b03f6b83449e716428a31bb1f
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-28 14:39:15 +0100

    Add: Lower Bound Function

--- 07. Binary Search/Day 40/03. first-and-last-occurrence-of-number.py
+++ 07. Binary Search/Day 40/03. first-and-last-occurrence-of-number.py
@@ -24,6 +24,20 @@ def first_and_last_occurrence_brute(arr,n,x):
 # Time Complexity = O(n)
 # Space Complexity = O(1)
 
+# Binary Search
+def lower_bound(arr,n,x):
+    low = 0
+    high = n - 1
+    ans = n
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] >= x:
+            ans = mid
+            high = mid - 1
+        else:
+            low = mid + 1     
+    return ans
+        
 
 
 
