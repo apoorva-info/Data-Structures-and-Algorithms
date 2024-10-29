d1f02d94038c1864438e3d7c5ec38ace571b3265
apoorva-info <apoorvaagupta.info@gmail.com>
2024-10-29 15:09:26 +0100

    Updated: Binary Search Solution

--- 07. Binary Search/Day 41/01. search-in-a-rotated-sorted-array.py
+++ 07. Binary Search/Day 41/01. search-in-a-rotated-sorted-array.py
@@ -12,16 +12,23 @@ def linear_search(arr,n,x):
 
 def binary_search(arr,n,x):
     low = 0
-    high = 1
+    high = n - 1
     while low <= high:
         mid = (low + high)//2
         if arr[mid] == x:
             return mid
-        elif arr[mid] < x:
-            high = mid + 1
+        if arr[low] <= arr[mid]:
+            if arr[low] <= x and x <= arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
         else:
-            low = mid - 1
+            if arr[mid] <= x and x <= arr[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
     return -1
+
 
 # User Input
 size = int(input(f"Enter the size of the sorted array: "))
