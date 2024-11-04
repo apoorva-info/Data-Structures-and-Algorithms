4ee06508ad2ae8c25824dc81837f51c82e7e0159
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-04 18:51:01 +0100

    Added: Brute Force Approach

---
+++ 07. Binary Search/Day 42/01. search-element-in-a-rotated-sorted-array.py
@@ -0,0 +1,23 @@
+# Goal: Given a rotated sorted array with duplicates and a target value, return True if target value is present in the array 
+# else return False.
+
+# Brute Force Approach
+def search_element_in_rotated_sorted_array_brute(arr,x):
+    for element in arr:
+        if element == x:
+            return True
+    return False
+    
+
+
+
+# User Input
+size = int(input(f"Enter the size of the sorted array: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the element at array[{i}]: "))
+    array.append(element)
+x = int(input("Enter the value of the target: "))
+
+result = search_element_in_rotated_sorted_array_brute(array,x)
+print(result)
