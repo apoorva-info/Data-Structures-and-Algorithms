7f8fd594eff926c626eddbe4b7facf1d2c2a6885
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-09 11:59:48 +0100

    Added: Optimal Approach using Binary Search

--- 07. Binary Search/Day 45/01. Find-peak-element.py
+++ 07. Binary Search/Day 45/01. Find-peak-element.py
@@ -2,7 +2,7 @@
 # Peak Element:  arr[i-1] < arr[i] > arr[i+1]
 
 # Brute Force Approach
-def peak_element(arr,n):
+def peak_element_brute(arr,n):
     for i in range(0,n):
         if i == 0:
             if arr[i] > arr[i+1]:
@@ -16,6 +16,18 @@ def peak_element(arr,n):
 # Time Complexity = O(n)
 # Space Complexity = O(1)
 
+# Optimal Approach
+def peak_element_optimal(arr,n):
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
+            return mid
+        if arr[mid] < arr[mid + 1]:
+            low = mid - 1
+        else:
+            high = mid + 1
 
 
 # User Input
@@ -26,5 +38,7 @@ for i in range(size):
     array.append(element)
 
 # Function Call
-result = peak_element(array,size)
+# result = peak_element_brute(array,size)
+# print(result)
+result = peak_element_optimal(array,size)
 print(result)
