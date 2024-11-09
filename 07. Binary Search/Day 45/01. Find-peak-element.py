273fb2afdb095b7d750359b6b4a639e64268b5ac
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-09 12:26:24 +0100

    Updated: Binary Search Logic

--- 07. Binary Search/Day 45/01. Find-peak-element.py
+++ 07. Binary Search/Day 45/01. Find-peak-element.py
@@ -26,16 +26,16 @@ def peak_element_optimal(arr,n):
         return arr[n-1]
     
     else:
-        low = 0
-        high = n - 1
+        low = 1
+        high = n - 2
         while low <= high:
             mid = (low + high)//2
             if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
                 return mid
             if arr[mid] > arr[mid - 1]:
-                low = mid - 1
+                low = mid + 1
             else:
-                high = mid + 1
+                high = mid - 1
 
 
 # User Input
