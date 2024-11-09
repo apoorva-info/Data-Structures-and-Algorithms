0379c21e957aa619ed806a64b0e36e15eef2f4e2
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-09 12:16:37 +0100

    Added: Edge Case 1

--- 07. Binary Search/Day 45/01. Find-peak-element.py
+++ 07. Binary Search/Day 45/01. Find-peak-element.py
@@ -18,16 +18,19 @@ def peak_element_brute(arr,n):
 
 # Optimal Approach
 def peak_element_optimal(arr,n):
-    low = 0
-    high = n - 1
-    while low <= high:
-        mid = (low + high)//2
-        if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
-            return mid
-        if arr[mid] < arr[mid + 1]:
-            low = mid - 1
-        else:
-            high = mid + 1
+    if (i == 0 or arr[i] > arr[i - 1]) and (i == n - 1 or arr[i] > arr[i + 1]):
+        return arr[i]
+    else:
+        low = 0
+        high = n - 1
+        while low <= high:
+            mid = (low + high)//2
+            if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
+                return mid
+            if arr[mid] > arr[mid - 1]:
+                low = mid - 1
+            else:
+                high = mid + 1
 
 
 # User Input
