027e1e34e102361d5914aa9964774c452cedaab8
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-20 22:15:19 +0100

    Added: Koko eating bananas problem

---
+++ 07. Binary Search/Day 47/01. Koko-eating-bananas.py
@@ -0,0 +1,19 @@
+# Goal: Find the minimum integer k such that Koko can eat all bananas within h hours.
+
+def koko_eating_bananas_brute(pile,h):
+    
+
+    return k
+
+# User Input
+size = int(input("Enter the size of the array: "))
+h = int(input("Enter the value of h: "))
+pile = []
+for i in range(size):
+    element = int(input(f"Enter the no. of bananas in {i + 1} pile: "))
+    pile.append(element)
+print(pile)
+
+# Function Call
+result = koko_eating_bananas_brute(pile,h)
+print(result)
