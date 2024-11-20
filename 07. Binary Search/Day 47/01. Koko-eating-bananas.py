11f3502ede73e16ebca4131c9896f810fa3e8ad2
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-20 22:48:04 +0100

    Added: Better Approach using Binary Search

--- 07. Binary Search/Day 47/01. Koko-eating-bananas.py
+++ 07. Binary Search/Day 47/01. Koko-eating-bananas.py
@@ -4,19 +4,34 @@ import math
 
 def func(arr,hours):
     total_hours = 0
-    for i in range(len(pile)):
+    for i in range(len(arr)):
         total_hours += math.ceil(arr[i]/hours)
-        return total_hours    
+    return total_hours    
     
 def koko_eating_bananas_brute(pile,h):
     max_num = max(pile)
-    for i in range(max_num):
+    for i in range(1,max_num+1):
         required_time = func(pile,i)
         if required_time <= h:
             return i
 # Time Complexity: O(max(arr)*n)
 # Space Complexity: O(1)
         
+def koko_eating_bananas_better(pile,h):
+    high = max(pile)
+    low = 1
+    ans = float('inf')
+    while low <= high:
+        mid = (low + high)//2
+        required_time = func(pile,mid)
+        if required_time > h:
+            low = mid + 1
+        elif required_time <= h:
+            ans = mid
+            high = mid - 1
+    return ans
+# Time Complexity: O(log(max(arr))*n)
+# Space Complexity: O(1)
 
 # User Input
 size = int(input("Enter the size of the array: "))
@@ -30,3 +45,5 @@ print(pile)
 # Function Call
 result = koko_eating_bananas_brute(pile,h)
 print(result)
+result2 = koko_eating_bananas_better(pile,h)
+print(result2)
