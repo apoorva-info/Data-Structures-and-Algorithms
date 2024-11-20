880e54bdac42c4e68af060dacaede292f0e083e4
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-20 22:20:08 +0100

    Completed: Brute Force Approach

--- 07. Binary Search/Day 47/01. Koko-eating-bananas.py
+++ 07. Binary Search/Day 47/01. Koko-eating-bananas.py
@@ -1,9 +1,22 @@
 # Goal: Find the minimum integer k such that Koko can eat all bananas within h hours.
 
-def koko_eating_bananas_brute(pile,h):
-    
+import math
 
-    return k
+def func(arr,hours):
+    total_hours = 0
+    for i in range(len(pile)):
+        total_hours += math.ceil(arr[i]/hours)
+        return total_hours    
+    
+def koko_eating_bananas_brute(pile,h):
+    max_num = max(pile)
+    for i in range(max_num):
+        required_time = func(pile,i)
+        if required_time <= h:
+            return i
+# Time Complexity: O(max(arr)*n)
+# Space Complexity: O(1)
+        
 
 # User Input
 size = int(input("Enter the size of the array: "))
