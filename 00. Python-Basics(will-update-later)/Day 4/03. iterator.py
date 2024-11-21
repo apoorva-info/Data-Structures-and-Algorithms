68eddf1fda8f13bd70680fbbd91b57a351dbe3c3
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-21 18:12:15 +0100

    Updated: StopIteration

--- 00. Python-Basics(will-update-later)/Day 4/03. iterator.py
+++ 00. Python-Basics(will-update-later)/Day 4/03. iterator.py
@@ -13,14 +13,15 @@ class MyNumbers:
         self.a = 1
         return self
     def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
 
 myclass = MyNumbers()
 myiter = iter(myclass)
 
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
+for i in myiter:
+    print(i)
