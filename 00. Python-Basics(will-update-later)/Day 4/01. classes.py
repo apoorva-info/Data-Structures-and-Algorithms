dc994209427b67a2aeb56b13455e6b5c88edc3c0
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-21 15:39:07 +0100

    Added: Basic Concept of Classes

---
+++ 00. Python-Basics(will-update-later)/Day 4/01. classes.py
@@ -0,0 +1,7 @@
+# Class is like an object constructor or a "blueprint" for creating objects.
+
+class MyClass: # Class
+    x = "Hello world!" # x is the property.
+    
+p1 = MyClass() # p1 is the object.
+print(p1.x)
