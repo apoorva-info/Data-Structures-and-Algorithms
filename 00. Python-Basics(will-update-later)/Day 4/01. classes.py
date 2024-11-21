e1460ed8fc5214aef95a6fa7c8c232e55164e2ad
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-21 16:04:41 +0100

    Updated: __init__() Function

--- 00. Python-Basics(will-update-later)/Day 4/01. classes.py
+++ 00. Python-Basics(will-update-later)/Day 4/01. classes.py
@@ -5,3 +5,20 @@ class MyClass: # Class
     
 p1 = MyClass() # p1 is the object.
 print(p1.x)
+
+# __init__() Function:
+# Magic Method 
+# Used to initialize object attributes
+# Automatically called when you create an instance of a class
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+person1 = Person("xyz",22)
+person2 = Person("abc",24)
+
+print(person1.name)
+print(person2.age)
+
+        
