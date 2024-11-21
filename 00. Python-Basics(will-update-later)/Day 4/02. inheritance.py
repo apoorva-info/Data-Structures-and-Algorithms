31fdc312209fcfc7defa97c0e816d9642997ea2f
apoorva-info <apoorvaagupta.info@gmail.com>
2024-11-21 17:16:29 +0100

    Updated: Child Class

--- 00. Python-Basics(will-update-later)/Day 4/02. inheritance.py
+++ 00. Python-Basics(will-update-later)/Day 4/02. inheritance.py
@@ -2,6 +2,7 @@
 # Parent Class: Class being inherited from
 # Child Class: Class that inherits from another class
 
+# Parent Class
 class Person:
     def __init__(self,first_name,last_name):
         self.firstname = first_name
@@ -11,5 +12,25 @@ class Person:
     def printname(self):
         print(f"The name is {self.firstname} {self.lastname}.")
     
-x = Person("xyz","abc")
-x.printname()
+# x = Person("xyz","abc")
+# x.printname()
+
+# Child Class
+class Student(Person):
+    # def __init__(self, first_name, last_name): 
+    # The child class will not inherit the properties from parent class
+    # It would override the inheritance of the parent's init function 
+    def __init__(self, first_name, last_name,year):
+        Person.__init__(self,first_name, last_name) # super function can also be used
+        self.graduationyear = year
+    
+    def printyear(self):
+        print(f"The year of graduation is {self.graduationyear}.")
+        
+
+
+        
+
+y = Student("John","String", 2009)
+y.printname()
+y.printyear()
