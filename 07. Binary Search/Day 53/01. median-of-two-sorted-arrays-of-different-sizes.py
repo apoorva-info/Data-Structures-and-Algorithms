39839bbbf3d9d93c2b018dbe47b58255521b4004
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-05 20:44:32 +0100

    Update: Better Approach

--- 07. Binary Search/Day 53/01. median-of-two-sorted-arrays-of-different-sizes.py
+++ 07. Binary Search/Day 53/01. median-of-two-sorted-arrays-of-different-sizes.py
@@ -2,7 +2,6 @@
 
 # Brute Force Approach
 def find_median_brute(arr1,arr2,n1,n2):
-
     i = 0 
     j = 0
     arr = []
@@ -32,6 +31,53 @@ def find_median_brute(arr1,arr2,n1,n2):
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
+# Better Approach
+def find_median_better(arr1,arr2,n1,n2):
+    i = 0
+    j = 0
+    n = n1 + n2
+    ind2 = n//2
+    ind1 = ind2 - 1
+    count = 0
+    ind1_element = -1 
+    ind2_element = -1
+
+    while (i < n1 and j < n2):
+        if arr1[i] < arr2[j]:
+            if count == ind1:
+                ind1_element = arr1[i]
+            elif count == ind2:
+                ind2_element = arr1[i]
+            count += 1
+            i += 1
+        else:
+            if count == ind1:
+                ind1_element = arr2[j]
+            elif count == ind2:
+                ind2_element = arr2[j]
+            count += 1
+            j += 1
+        while(i < n1):
+            if count == ind1:
+                ind1_element = arr1[i]
+            elif count == ind2:
+                ind2_element = arr1[i]
+        while(j < n2):
+            if count == ind1:
+                ind1_element = arr2[j]
+            elif count == ind2:
+                ind2_element = arr2[j]
+        
+        if n % 2 == 0:
+            return (ind1_element + ind2_element)//2
+        else:
+            return ind2_element
+        
+
+
+
+
+    
 # User Input:
 size1 = int(input("Enter the size of array 1: "))
 arr1 = []
@@ -48,5 +94,7 @@ for i in range(size2):
 arr2 = sorted(arr2)
 
 # Function Call 
-result = find_median_brute(arr1,arr2,size1,size2)
+# result = find_median_brute(arr1,arr2,size1,size2)
+# print(result)
+result = find_median_better(arr1,arr2,size1,size2)
 print(result)
