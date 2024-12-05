f647fcb7bf207cc0586b33be6926d45e492a9758
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-05 20:14:23 +0100

    Update: Used Merge Sort to sort the arrays instead of using built-in function.

--- 07. Binary Search/Day 53/01. median-of-two-sorted-arrays-of-different-sizes.py
+++ 07. Binary Search/Day 53/01. median-of-two-sorted-arrays-of-different-sizes.py
@@ -1,17 +1,35 @@
 # Goal: To find the median of two sorted arrays of different sizes.
 
 # Brute Force Approach
-def find_median_brute(arr1,arr2):
-    arr = arr1 + arr2
-    arr = sorted(arr)
+def find_median_brute(arr1,arr2,n1,n2):
+
+    i = 0 , j = 0
+    arr = []
+    while i < n1 & j < n2:
+        if arr1[i] < arr2[j]:
+            arr.append(arr[i])
+            i += 1
+        else:
+            arr.append(arr[j])
+            j += 1
+    while i < n1:
+        arr.append(arr1[i])
+        i += 1
+    while j < n2:
+        arr.append(arr2[j])
+        j += 1 
+    print(arr)
+
+
+
     n = len(arr)
     print(arr)
 
     if n % 2 == 0:
-        median = (arr[(n/2)] + arr[(n/2)+1])/2
+        median = (arr[n/2] + arr[(n/2)-1])/2
         return median
     else:
-        median = arr[n/2]
+        median = arr[(n//2)]
         return median
 
 
@@ -31,5 +49,5 @@ for i in range(size2):
 arr2 = sorted(arr2)
 
 # Function Call 
-result = find_median_brute(arr1,arr2)
+result = find_median_brute(arr1,arr2,size1,size2)
 print(result)
