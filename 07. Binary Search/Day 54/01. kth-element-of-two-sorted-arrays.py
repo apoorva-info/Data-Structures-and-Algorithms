839fa550151432f8cfe2666b94ef5c2d01d5ebcd
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-06 21:04:15 +0100

    Update: Better Approach

--- 07. Binary Search/Day 54/01. kth-element-of-two-sorted-arrays.py
+++ 07. Binary Search/Day 54/01. kth-element-of-two-sorted-arrays.py
@@ -26,6 +26,36 @@ def find_element_brute(arr1,arr2,n1,n2,k):
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
+# Better Approach
+def find_element_better(arr1,arr2,n1,n2,k):
+    if n1 > n2:
+        return find_element_better(arr2,arr1,n2,n1,k)
+    left = k
+    low = max(0,k-n2)
+    high = min(k,n1)
+    while low <= high:
+        mid1 = (low + high)//2
+        mid2 = left - mid1
+        l1 = float('-inf')
+        l2 = float('-inf')
+        r1 = float('inf')
+        r2 = float('inf')
+        if mid1 < n1:
+            r1 = arr1[mid1]
+        if mid2 < n2:
+            r2 = arr2[mid2]
+        if mid1 - 1>= 0:
+            l1 = arr1[mid1-1]
+        if mid2 - 1>= 0:
+            l2 = arr2[mid2-1]
+        if l1 <= r2 and l2 <= r1:
+            return max(l1,l2)
+        elif l1>r2:
+            high = mid1 - 1
+        else:
+            low = mid1 + 1
+    return 0
+
 
 # User Input:
 size1 = int(input("Enter the size of array 1: "))
@@ -45,5 +75,7 @@ arr2 = sorted(arr2)
 k = int(input("Enter the value of the k-th element: "))
 
 # Function Call
-result = find_element_brute(arr1,arr2,size1,size2,k)
+# result = find_element_brute(arr1,arr2,size1,size2,k)
+# print(result)
+result = find_element_better(arr1,arr2,size1,size2,k)
 print(result)
