37b73e953d54ce8f10afbe52186e6c34fbb1ea67
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-12 19:10:34 +0100

    Add: Find the row with maximum number of 1's.

---
+++ 07. Binary Search/Day 55/01. find-the-row-with-maximum-number-of-1s.py
@@ -0,0 +1,14 @@
+# Goal: To find the row with the maximum number of 1's.
+
+
+
+# User Input
+import numpy as np
+m = int(input("Enter the number of rows:"))
+n = int(input("Enter the number of columns: "))
+
+array = np.empty((m,n))
+for i in range(m):
+    for j in range(n):
+        array[i,j] = int(input(f"Enter the element at array[{i}][{j}]: "))
+print(array)
