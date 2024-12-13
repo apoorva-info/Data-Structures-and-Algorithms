23c3a6fb90d7f29b4f7523dcd02680fb5667e2ab
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-13 21:18:45 +0100

    Update: Optimized the solution

--- 07. Binary Search/Day 56/01. search-in-a-2D-matrix.py
+++ 07. Binary Search/Day 56/01. search-in-a-2D-matrix.py
@@ -14,16 +14,17 @@ def find_element_brute(arr,m,n,target):
 # Better Approach
 def find_element_better(arr,m,n,target):
     for i in range(m):
-        low = 0
-        high = n - 1
-        while low <= high:
-            mid = (low + high)//2
-            if arr[i][mid]== target:
-                return True
-            elif arr[i][mid] < target:
-                low = mid + 1
-            else: 
-                high = mid - 1
+        if arr[i][0] <= target and arr[i][n-1] >= target:
+            low = 0
+            high = n - 1
+            while low <= high:
+                mid = (low + high)//2
+                if arr[i][mid]== target:
+                    return True
+                elif arr[i][mid] < target:
+                    low = mid + 1
+                else: 
+                    high = mid - 1
     return False
 
 
