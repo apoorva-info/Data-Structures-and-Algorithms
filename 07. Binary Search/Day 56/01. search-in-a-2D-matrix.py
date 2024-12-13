082f63f9d4c3ebcf3129c296965513d8b82fbe06
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-13 20:57:56 +0100

    Add: Search in a 2D matrix problem

---
+++ 07. Binary Search/Day 56/01. search-in-a-2D-matrix.py
@@ -0,0 +1,16 @@
+# Goal: To search an element in a 2D matrix.
+
+# User Input
+import numpy as np
+m = int(input("Enter the number of rows in a matrix: "))
+n = int(input("Enter the number of columns in a matrix: "))
+
+array = np.empty((m,n), dtype=int)
+for i in range(m):
+    for j in range(n):
+        array[i][j] = int(input(f"Enter the element at array{[i]}{[j]}: "))
+print(array)
+
+# Function Call
+result = 
+print(result)
