8141245a3142f6d1e85a02e5aa5bdda0e2a4cc78
apoorva-info <apoorvaagupta.info@gmail.com>
2024-12-30 15:09:35 +0100

    Add: Find median of a 2 D matrix

---
+++ 07. Binary Search/Day 59/01. matrix-median.py
@@ -0,0 +1,30 @@
+# Goal: To find the median of a row-wise sorted matrix.
+
+# Brute Force Approach
+def find_median_brute(arr,m,n):
+    ls = []
+    for i in range(m):
+        for j in range(n):
+            ls.append(arr[i][j])
+    ls = sorted(ls)
+    median = (m + n)//2
+    return ls[median]
+    
+
+
+# User Input
+m = int(input("Enter the number of rows: "))
+n = int(input("Enter the number of columns: "))
+
+import numpy as np
+
+array = np.empty((m,n),dtype=int)
+
+for i in range(m):
+    for j in range(n):
+        array[i][j] = int(input(f"Enter the element at array[{i}][{j}]: "))
+print(array)
+
+# Function Call
+result = find_median_brute(array,m,n)
+print(result)
