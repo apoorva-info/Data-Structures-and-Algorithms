354bf56c00216841308e496d4bb9bd74e707991d
apoorva-info <apoorvaagupta.info@gmail.com>
2025-01-06 21:26:59 +0100

    Modelling a Bank Account

--- 08. Linked-List.py/00. oops-concept-revision.py
+++ 08. Linked-List.py/00. oops-concept-revision.py
@@ -48,8 +48,41 @@ class Dog:
 
     def bark(self):
         print(f"{self.name} says woof.")
-        
+
 # Creating objects
 dog1 = Dog("Little", 10)
 dog1.bark()
-         
+
+dog2 = Dog("Lucy", 4)
+dog2.bark()
+
+
+## Modelling a Bank Account
+
+# Tasks: 1. Deposit 2. Withdraw 3. Balance Amount
+
+class BankAccount:
+    def __init__(self,name,balance=0):
+        self.name = name
+        self.balance = balance
+    
+    def deposit(self,amount):
+        self.balance += amount
+        print(f"{amount} is deposited. New balance = {self.balance}")
+
+    def withdraw(self, amount):
+        if amount < self.balance:
+            self.balance -= amount
+            print(f"{amount} is withdrawn. New balance = {self.balance} ")
+        else:
+            print("Insufficient Balance")
+
+    def get_balance(self):
+        return self.balance
+
+
+# creating object
+customer1 = BankAccount("Alice")
+customer1.deposit(1000)
+customer1.withdraw(200)
+print(customer1.get_balance())        
