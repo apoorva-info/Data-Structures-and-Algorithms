8c0ce9c508c1304f3a319db7f13f84de94bd903f
apoorva-info <apoorvaagupta.info@gmail.com>
2025-01-07 22:56:33 +0100

    Child Class

--- 08. Linked-List/01. inheritance-in-python.py
+++ 08. Linked-List/01. inheritance-in-python.py
@@ -3,7 +3,6 @@
 
 
 # Parent Class
-
 class Car:
     def __init__(self,windows,doors,enginetype):
         self.windows=windows
@@ -16,4 +15,26 @@ class Car:
 car1 = Car(4,4,"Petrol")
 car1.drive()
 
-        
+# Child Class --> Wrong way
+# class Tesla(Car): # You will writing the parent class within the brackets.
+#     def color(self):
+#         print(f"The color of the car is blue.")
+
+# car2 = Tesla(4,5,"Petrol")
+# car2.color()
+
+# Child Class
+class Tesla(Car): # You will writing the parent class within the brackets.
+    # Constructor
+    def __init__(self, windows, doors, enginetype,is_selfdriving):
+        # since car already has these attributes, we don't need to write the self.xyz , instead I can directly use super keyword
+        super().__init__(windows,doors,enginetype) # From the parent class, I am calling my init method
+        self.is_selfdriving= is_selfdriving
+    
+    def selfdriving(self):
+        print(f"Tesla supports self driving: {self.is_selfdriving}")
+
+
+
+car2 = Tesla(4,5,"Electric",True)
+car2.selfdriving()
