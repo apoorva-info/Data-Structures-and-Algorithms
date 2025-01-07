7061a7d4314c8106aa14879628433e259c62309d
apoorva-info <apoorvaagupta.info@gmail.com>
2025-01-07 22:58:47 +0100

    Single Inheritance

--- 08. Linked-List/01. inheritance-in-python.py
+++ 08. Linked-List/01. inheritance-in-python.py
@@ -23,18 +23,18 @@ car1.drive()
 # car2 = Tesla(4,5,"Petrol")
 # car2.color()
 
+# Single Inheritance
 # Child Class
 class Tesla(Car): # You will writing the parent class within the brackets.
     # Constructor
     def __init__(self, windows, doors, enginetype,is_selfdriving):
-        # since car already has these attributes, we don't need to write the self.xyz , instead I can directly use super keyword
+        # since car already has these attributes, we don't need to write the self.xyz , instead we can directly use super keyword
         super().__init__(windows,doors,enginetype) # From the parent class, I am calling my init method
         self.is_selfdriving= is_selfdriving
     
     def selfdriving(self):
         print(f"Tesla supports self driving: {self.is_selfdriving}")
 
-
-
 car2 = Tesla(4,5,"Electric",True)
 car2.selfdriving()
+car2.drive()
