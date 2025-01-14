bc22dc4e06bc451617ddaf2b67c64ec3bdafaf8b
apoorva-info <apoorvaagupta.info@gmail.com>
2025-01-14 18:18:41 +0100

    Encapsulation

---
+++ 08. Linked-List/03. encapsulation.py
@@ -0,0 +1,15 @@
+# Encapsulation:
+# Concept of wrapping data(variables) and methods(functions) together as a single unit.
+# Restricts direct access to some of the object's components, which is a means of preventing accidental interference and misuse of data.
+
+
+# Encapsulation with Getter and Setter Methods
+# Public, Private, Protected or access modifiers
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+person = Person("Jack",23)
+print(person.name)
